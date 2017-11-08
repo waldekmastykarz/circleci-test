0469f63bdbf6a514b64cb739fbea3c19bc012e88
Waldek Mastykarz <waldek@mastykarz.nl>
2017-11-08 19:28:23 +0100

    .

--- scripts/update-package-version.js
+++ scripts/update-package-version.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 const path = require('path');
 const packageJson = require('../package.json');
-packageJson.version += `-beta+${process.argv[process.argv.length-1].substr(0, 7)}`;
+packageJson.version += `-beta.${process.argv[process.argv.length-1].substr(0, 7)}`;
 fs.writeFileSync(path.join(path.resolve('.'), 'package.json'), JSON.stringify(packageJson, null, 2));
